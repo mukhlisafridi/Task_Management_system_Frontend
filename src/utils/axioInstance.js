0097cbbd145614e4c2a210e069963d0797ac7100
mukhlisafridi <mukhlisafridi4@gmail.com>
2025-12-03 15:32:22 +0500

    completed

--- src/utils/axioInstance.js
+++ src/utils/axioInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // ✅ CORRECT BASE URL - /api included
-const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = "https://task-management-system-backend-beta.vercel.app";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
